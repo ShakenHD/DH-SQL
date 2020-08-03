23d9fde2a108127d59b22981ed2d855099e3e536
Daniel Hockley <danhockley1@gmail.com>
2020-08-03 13:50:03 +0200

    first commit

---
+++ chapter1.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE dans_list;
+use dans_list;
+
+CREATE TABLE doughnut_list
+(
+doughnut_name VARCHAR(10),
+doughnut_type VARCHAR(6)
+);
+
+CREATE TABLE my_contacts
+(
+last_name VARCHAR(30),
+first_name VARCHAR(20),
+email VARCHAR(50),
+birthday DATE,
+profession VARCHAR(50),
+location VARCHAR(50),
+status VARCHAR(20),
+interests VARCHAR(100),
+seeking VARCHAR(100)
